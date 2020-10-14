c949233109d71651902dc62486dfc673563a7ed8
Thomas Graft <33382058+ThomasGraft@users.noreply.github.com>
2020-10-14 13:29:46 -0400

    Added data bindings, code-behind class, and razor component class

---
+++ Blazor/Split.razor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Advanced.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace Advanced.Blazor
+{
+    public partial class Split
+    {
+        [Inject]
+        public DataContext Context { get; set; }
+
+        public IEnumerable<string> Names =>
+            Context.People.Select(p => p.FirstName);
+    }
+}
